c6e20c422eed18f3377edcd622e17219d6c1afed
oyakovenko <alex231@ukr.net>
2023-09-19 22:08:58 +0300

    send email using form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,13 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
+
+with st.form(key='email'):
+    email_adress = st.text_input("Your email adress")
+    message = st.text_area("Your message")
+    message = message + "\n" + email_adress
+    button = st.form_submit_button("Submit")
+    if button:
+        send_email(message)
+        st.info("email was sent successfully")
---
+++ send_email.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def send_email(message):
+    smtp_server = "smtp.gmail.com"
+    port = 587
+    password = "liaq grpx ysjo endi "
+    sender_email = "oyakovenko.it@gmail.com"
+    receiver_email = "oyakovenko.it@gmail.com"
+    subject = "Email from portfolio web site"
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.starttls()
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
+
+
