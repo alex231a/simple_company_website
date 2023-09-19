f0f658c7489e5db7ed3439eeabdcd9f20dd44cce
oyakovenko <alex231@ukr.net>
2023-09-19 22:58:22 +0300

    done

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,13 +1,18 @@
 import streamlit as st
 from send_email import send_email
+import pandas as pd
+
+list_m = []
+df = pd.read_csv('topics.csv')
 
 st.header("Contact Me")
 
 with st.form(key='email'):
     email_adress = st.text_input("Your email adress")
+    topic = st.selectbox('What topic would you like to discuss?', (df['topic']))
     message = st.text_area("Your message")
     message = message + "\n" + email_adress
     button = st.form_submit_button("Submit")
     if button:
-        send_email(message)
+        send_email(message, topic)
         st.info("email was sent successfully")
--- send_email.py
+++ send_email.py
@@ -3,7 +3,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-def send_email(message):
+def send_email(message, topic):
     smtp_server = "smtp.gmail.com"
     port = 587
     password = "liaq grpx ysjo endi "
@@ -14,6 +14,7 @@ def send_email(message):
     msg['From'] = sender_email
     msg['To'] = receiver_email
     msg['Subject'] = subject
+    msg['Topic'] = topic
     msg.attach(MIMEText(message, 'plain'))
     with smtplib.SMTP(smtp_server, port) as server:
         server.starttls()
